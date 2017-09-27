77601adf816e2e69d5da3c6f04e4ac9c372df143
radetsky <radetsky@outlook.com>
2017-09-27 11:59:30 +0300

    First commit

---
+++ BinarySearchTree/BaseClass.cs
@@ -0,0 +1,44 @@
+﻿namespace DotNet
+{
+    public class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node(int data)
+        {
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+
+    public class Bst
+    {
+        Node _root;
+        static int _count;
+
+        public Bst()
+        { 
+            _root = null;
+            _count = 0;
+        }
+
+        public Node AddNode(int data)
+        {
+            var newNode = new Node(data);
+            if (_root == null)
+            {
+                _root = newNode;
+            }
+            else if (data < _root.Data)
+            {
+                newNode.Left = new Node(data);
+            }
+            else newNode.Right = new Node(data);
+            _count++;
+            return _root;
+        }
+    }
+}
